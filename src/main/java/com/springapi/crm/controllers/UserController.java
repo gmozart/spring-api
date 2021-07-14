ad080a1e24b8378bdd72f321c3baed3162b66f7c
Gleison Mozart <mozart.gleison@gmail.com>
2021-07-13 22:49:28 -0300

    Criada notation post e metodo list users

--- src/main/java/com/springapi/crm/controllers/UserController.java
+++ src/main/java/com/springapi/crm/controllers/UserController.java
@@ -1,6 +1,11 @@
 package com.springapi.crm.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +14,9 @@ import com.springapi.crm.model.User;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+	
+	
+	public List<User> users = new ArrayList<>();
 
 	@GetMapping("/")
 	public User teste() {
@@ -21,5 +29,20 @@ public class UserController {
 		
 		return user;
 	}
+	
+	@PostMapping("/")
+	public User user(@RequestBody User user) {
+		
+		users.add(user);
+		
+		return user;
+	}
+	
+	@GetMapping("/list")
+    public List<User> list(){
+		
+		
+		return users;
+	}	
 	
 }
