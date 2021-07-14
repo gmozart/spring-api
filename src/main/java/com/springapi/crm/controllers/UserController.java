3c41d164484321f258f5161721521b2070b0def2
Gleison Mozart <mozart.gleison@gmail.com>
2021-07-14 00:13:11 -0300

    Delete funcionando

--- src/main/java/com/springapi/crm/controllers/UserController.java
+++ src/main/java/com/springapi/crm/controllers/UserController.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springapi.crm.model.User;
@@ -27,16 +29,17 @@ public class UserController {
 	@GetMapping("/{id}")
 	public User user(@PathVariable("id") Long id) {
 		
-		Optional<User> userFind = users.stream().filter(user -> user.getId() == id).findFirst(); 
-				
-		if(userFind.isPresent()) {
-			
+		
+		Optional<User> userFind = this.userRepo.findById(id);
+		
+		if(userFind.isPresent()){
 			
 			return userFind.get();
 		}
 		
 		
-		return null;
+			return null;
+	
 	}
 	
 	@PostMapping("/")
@@ -51,6 +54,15 @@ public class UserController {
 		
 		
 		return this.userRepo.findAll();
-	}	
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		
+		userRepo.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
+		
+	}
 	
 }
